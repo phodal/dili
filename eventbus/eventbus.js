633ff0be94076e555e7b9df17d49dd75aba21c24
Phodal Huang <h@phodal.com>
2017-11-20 16:15:39 +0800

    [T] thinking in eventbus

---
+++ eventbus/eventbus.js
@@ -0,0 +1,17 @@
+let EventBusClass = {};
+EventBusClass = function() {
+
+};
+
+EventBusClass.prototype = {
+  addEventListener: function(type, callback, scope) {
+  },
+  removeEventListener: function(type, callback, scope) {
+  },
+  hasEventListener: function(type, callback, scope) {
+  },
+  dispatch: function(type, target) {
+  },
+  getEvents: function() {
+  }
+}
